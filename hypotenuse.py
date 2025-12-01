7a0b52422b807febeae01969416f739bdb922fc9
Rysin Mikhail <rysin@mirea.ru>
2025-12-01 21:00:17 +0300

    Revert "Init commit"
    This reverts commit 16cd266afcd8eed7599f916d8726e1a07ea4ce66.

--- hypotenuse.py
+++ hypotenuse.py
@@ -4,8 +4,6 @@ def get_hypotenuse(a, b):
     return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 
 if __name__ == "__main__":
-    print("Введите a:")
-    a = int(input())
-    print("Введите b:")
-    b = int(input())
-    print("c =", get_hypotenuse(a,b))
+    a = 1
+    b = 2
+    print(get_hypotenuse(a,b))
